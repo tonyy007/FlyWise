2b316aba6f65b10915348c6dcd4a2619cb740210
tonyy007 <tonyyang1934@hotmail.com>
2023-07-03 21:48:35 -0500

    Logo added

--- flywise/src/App.jsx
+++ flywise/src/App.jsx
@@ -45,9 +45,9 @@ export default function Example() {
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
                       <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
+                        className="h-9 w-9"
+                        src="flywiseLogo.png"
+                        alt="flywise logo"
                       />
                     </div>
                     <div className="hidden md:block">
