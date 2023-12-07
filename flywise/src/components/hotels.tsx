25fd7d48d78da4530d923a5f058bfbea119aa689
Sergio Herrera <sergio.herrera02@utrgv.edu>
2023-12-06 21:03:16 -0600

    api included

--- flywise/src/components/hotels.tsx
+++ flywise/src/components/hotels.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import NavBar from "./navbar.tsx";
+import { useState } from "react";
 
-const Flights = () => {
+
+const Hotels = () => {
+    const [search, setSearch] = useState("");
+    async function getHotels() {
+        const url = `https://hotels4.p.rapidapi.com/locations/v3/search?q=${search}&locale=en_US&langid=1033&siteid=300000001`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '98c4e9a3a4mshb28905e99fd9166p1e889bjsn55ebe4f9e7e6',
+                'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+            }
+        };
+    
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+            return result
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <>
             <div className="min-h-full">
                 <hr></hr>
                 <main>
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex">
+                        <input type="text" onChange={((e) => { setSearch(e.target.value) })} placeholder='Insert Name of City' id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                        <button type="submit" onClick={getHotels} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    </div>
                     <div className="flex justify-between mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 ">
                         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
@@ -51,4 +76,4 @@ const Flights = () => {
     );
 };
 
-export default Flights;
+export default Hotels;
