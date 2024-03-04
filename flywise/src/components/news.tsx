d0f6d57f31f9b55614517b3255a4afb5df0cec17
tonyy007 <tonyyang1934@hotmail.com>
2024-03-04 15:21:24 -0600

    Format improved

--- flywise/src/components/news.tsx
+++ flywise/src/components/news.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import NavBar from "./navbar.tsx";
 import { useState } from 'react';
 
-const NewsCard = ({title, desciprtion, url, imageUrl, index}) =>(
-    <div className="flex justify-between mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 grid grid-cols-3 gap-4 ">
+const NewsCard = ({title, description, url, imageUrl, index}) =>(
         <div key={index} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <img className="p-8 rounded-t-lg" src={imageUrl} alt={title}/>
             <h3 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h3>
-            {/* <p>{desciprtion}</p> */}
-            <a href={url} target="_blank" rel="noopener noreferrer">Read more</a>
+            <p>{description}</p>
+            <a href={url} target="_blank" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-0.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" rel="noopener noreferrer">Read more</a>
         </div>
-    </div>
 );
 
 const News = () => {
@@ -64,16 +62,19 @@ const News = () => {
             {newsInfo && (
                 <div>
                 <h2>News Information for {city}</h2>
-                {newsInfo.articles.map((article, index) => (
-                    <NewsCard
-                    key={index}
-                    index={index}
-                    title={article.title}
-                    description={article.description}
-                    url={article.url}
-                    imageUrl={article.urlToImage}
-                    />
-                ))}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+
+                        {newsInfo.articles.map((article, index) => (
+                            <NewsCard
+                            key={index}
+                            index={index}
+                            title={article.title}
+                            description={article.description}
+                            url={article.url}
+                            imageUrl={article.urlToImage}
+                            />
+                        ))}
+                    </div>
                 </div>
             )}
 
