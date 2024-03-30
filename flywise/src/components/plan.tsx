5b453e12943a5508040d313ab3abc3dcd0a8e281
Yatin Gupta <yatingupta7777@gmail.com>
2024-03-30 14:18:42 -0700

    add plan layout

---
+++ flywise/src/components/plan.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Plan = () => {
+    return (
+        <>
+        <div>
+            <h1> Hello from Plan</h1>
+        </div>
+        </>
+    );
+};
+
+export default Plan;
